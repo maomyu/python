f2688f8deefb6224bb0c6570a3db50f8c94a351c
yuwei1 <yuweijiubayiqi@163.com>
2019-12-29 21:08:04 +0800

    返回函数

---
+++ day07/demo1.py
@@ -0,0 +1,16 @@
+# 返回函数
+# 可变参数的求和
+def calc_sum(*args):
+    ax = 0
+    for n in args:
+        ax = ax + n
+    return ax
+def lazy_sum(*args):
+    def sum():
+        ax = 0
+        for n in args:
+            ax = ax + n
+        return ax
+    return sum
+f = lazy_sum(1,3,5,7,9)
+print(f())
